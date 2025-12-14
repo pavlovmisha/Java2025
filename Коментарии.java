3c5be27afecbfd00a081633db1da7c0dc2b47c35
pavlovmisha <90309912+pavlovmisha@users.noreply.github.com>
2025-12-14 13:15:57 +0300

    Add files via upload

---
+++ Коментарии.java
@@ -0,0 +1,29 @@
+/**
+ * Класс, представляющий простого человека.
+ */
+public class Person {
+    
+    /**
+     * Имя человека.
+     */
+    private String name;
+
+    /**
+     * Конструктор.
+     * @param name имя
+     */
+    public Person(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Получить имя.
+     * @return имя
+     */
+    public String getName() {
+        return name;
+    }
+}
+
+
+
