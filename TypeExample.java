337e76f87d94fa3466e187b19e273b47e47880e8
pavlovmisha <90309912+pavlovmisha@users.noreply.github.com>
2025-12-14 12:26:55 +0300

    Add files via upload

---
+++ TypeExample.java
@@ -0,0 +1,13 @@
+public class StringPlusExample {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.5;
+        double result = a + b; // результат типа double
+        System.out.println("a + b = " + result);
+        
+        float f = 4.5f;
+        long l = 100L;
+        float result2 = f + l; // result3 — float
+        System.out.println("f + l = " + result2);
+    }
+}
