c3017a3b9dec3fdf1faa453ad6b2fd5f73f54079
pavlovmisha <90309912+pavlovmisha@users.noreply.github.com>
2025-12-14 12:09:55 +0300

    Add files via upload

---
+++ Printmisha.java
@@ -0,0 +1,10 @@
+public class PrintUnicodeName {
+    public static void main(String[] args) {
+        char m = '\u041C'; // М
+        char i = '\u0438'; // и
+        char sh = '\u0448'; // ш
+        char a = '\u0430';  // а
+
+        System.out.println("" + m + i + sh + a);
+    }
+}
