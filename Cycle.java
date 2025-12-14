9ce60d98df476366e102de4a007db5a0f8b5544b
pavlovmisha <90309912+pavlovmisha@users.noreply.github.com>
2025-12-14 16:27:09 +0300

    Add files via upload

---
+++ Cycle.java
@@ -0,0 +1,12 @@
+public class Cycle {
+    public static void main(String[] args) {
+        
+        while (true) {
+            System.out.println("Бесконечный цикл с while");
+        }
+        
+        do {
+            System.out.println("Бесконечный цикл с do-while");
+        } while (true);
+    }
+}
